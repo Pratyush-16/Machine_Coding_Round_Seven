c2806fd3057a1d28ef005cc09b76a4255831fa5c
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-07-09 21:53:24 +0530

    main page

--- seven/src/Pages/Main.jsx
+++ seven/src/Pages/Main.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react'
 import { DataContext } from '../Context/DataContext'
-
+import {data} from "../Data/Data"
+import { Link } from "react-router-dom";
 
 
 
 
 export default function Main() {
+    const { continents } = data;
 
     // const {state : {continents}} = useContext(DataContext)
 
@@ -16,6 +18,11 @@ export default function Main() {
         <h1>Welcome to Trip Advisor</h1>
         
         <h2>Top Continents for your next holiday</h2>
+
+        {continents.map((continent) =>
+
+         continent.name)
+         }
       
     </div>
   )
